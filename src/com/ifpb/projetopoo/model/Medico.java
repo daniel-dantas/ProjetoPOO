25f644da7da233335e1c06de36ae243f0382eb98
Ruan-Miguel <ruanmiguel14@hotmail.com>
2019-07-21 19:40:47 -0300

    adicao da classe medico

---
+++ src/com/ifpb/projetopoo/model/Medico.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ifpb.projetopoo.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Ruan
+ */
+public class Medico extends Pessoa{
+    private float salario;
+    private LocalDate dataAdmissao;
+    private List<String> especialidades;
+
+    public Medico(float salario, LocalDate dataAdmissao, List<String> especialidades, String cpf, String nome, LocalDate nascimento, Endereco endereco, Contato contato) {
+        super(cpf, nome, nascimento, endereco, contato);
+        this.salario = salario;
+        this.dataAdmissao = dataAdmissao;
+        this.especialidades = new ArrayList<>();
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public LocalDate getDataAdmissao() {
+        return dataAdmissao;
+    }
+
+    public void setDataAdmissao(LocalDate dataAdmissao) {
+        this.dataAdmissao = dataAdmissao;
+    }
+
+    public List<String> getEspecialidades() {
+        return especialidades;
+    }
+
+    public void addEspecialidade(String especialidade) {
+        this.especialidades.add(especialidade);
+    }
+    
+}
